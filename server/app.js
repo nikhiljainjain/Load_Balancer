35227011cd2e11a4eabc1935aa96167b85370070
NIKHIL JAIN <nikhiljain.ashajain@gmail.com>
2020-09-21 22:51:10 +0530

    feat: :building_construction: server code ready
    basic & simple server code is ready

---
+++ server/app.js
@@ -0,0 +1,38 @@
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+require("dotenv/config");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//best practices for security
+app.disable('etag');
+
+app.use('/', (req, res)=>res.json({	port: process.env.PORT }));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+console.log(`Server connected on the port ${process.env.PORT || 3000}`);
+
+module.exports = app;
